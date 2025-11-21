258cc78468a8b3774e768ad32d8f32f9dca78723
batool <batoolmao@gmail.com>
2025-11-21 12:09:09 +0500

    added more characters

--- init-data.sql
+++ init-data.sql
@@ -77,19 +77,94 @@ father of rosemary winters. courageous, determined, and resourceful under extrem
 'todd soley', 'hidenobu kiuchi', 'yaya chamki', '');
 
 update ingamecharacters
-set spriteURL = 'https://raw.githubusercontent.com/sushisajid/URLs/Games/GameLogos/InGameCharacters/ethan_re7.webp'
+set spriteURL = 'https://raw.githubusercontent.com/sushisajid/URLs/InGameCharacters/ethan_re7.webp'
 WHERE characterID = 1;
-
-select * from ingamecharacters;
-
--- appearances
 
 insert into Appearances (gameID, characterID, firstAppearance, isPlayable, notes) VALUES (
     1,
     currval('InGameCharacters_characterID_seq'),  -- gets the last inserted characterID
     true,
     true,
-    '- infected by mold, giving him enhanced resilience.\n- involved with major bioterrorism incidents in dulvey parish (baker family, eveline).\n- receives aid and guidance from zoe baker.\n- uses weapons including axes, chainsaws, and experimental bsaa equipment (albert-01).\n- dies heroically while saving family.'
+    'Ethan Winters appears as a rugged civilian in his early 30s, wearing a dark green jacket over a gray hoodie, blue jeans, and brown boots. He has short brown hair and a clean-shaven face. His outfit includes practical gear such as a flashlight and a weapons belt, and his animations display realistic weight and movement.'
 );
 
+insert into InGameCharacters (characterName, backstory, description, englishVA, japaneseVA, motionCapture, spriteURL) values
+('Mia Winters',
+'former operative for The Connections; caretaker of Eveline; wife of Ethan Winters and mother of Rosemary. presumed dead after the Annabelle incident, later recovered by the Baker family. captured and influenced by Eveline, attacks Ethan under her control, but eventually assists him in defeating Eveline. after the incident, enters witness protection in Europe with Ethan and their daughter.',
+'female, caucasian/american, born december 14, 1984; 170 cm (5''7"), 61 kg (134 lb). researcher for The Connections (2010 - 2014) and Eveline''s handler (2014). skilled in firearms and covert operations; protective, resourceful, and deeply committed to family.',
+'katie o''hagan', 'akari higuchi', 'N/A', 'https://raw.githubusercontent.com/sushisajid/URLs/InGameCharacters/mia_re7.webp');
+
+insert into InGameCharacters (characterName, backstory, description, englishVA, japaneseVA, motionCapture, spriteURL) values
+('David Anderson',
+'african-american deputy sheriff assigned to patrol the dulvey bayou following a series of disappearances. investigates the baker house, encounters ethan winters, but is killed by jack baker early in the incident. his corpse becomes partially converted by mold and is used by lucas baker to manipulate ethan. his death is later covered up by the sheriff’s office and bsaa as a gas leak.',
+'male, african-american, born c.1986; 175 cm (5''9"), 60 kg (130 lb). deputy sheriff by occupation; dutiful but skeptical and inexperienced when confronted with biohazard threats.',
+'hari williams', 'minoru kawai', 'pierre dager', 'https://raw.githubusercontent.com/sushisajid/URLs/InGameCharacters/david_re7.png');
+
+insert into InGameCharacters (characterName, backstory, description, englishVA, japaneseVA, motionCapture, spriteURL) values
+('Zoe Baker',
+'female from dulvey, louisiana; daughter of jack and marguerite baker, sister to lucas. involved in the dulvey incident as a captive resistant to eveline’s mind control. helps ethan winters navigate the baker estate, reattaches his hand, provides the genome codex, and guides him to cure her and mia. after being captured and partially calcified, she is rescued and quarantined, later gaining a new identity and working as a rookie reporter in new orleans while investigating the incident and the origins of eveline.',
+'female, caucasian/american, born c.1994; 170 cm (5''7"), 58 kg (130 lb). resilient, intelligent, resourceful; skilled at surviving in extreme circumstances and assisting others under duress.',
+'giselle gilbert', 'yu kobayashi', 'giselle gilbert', 'https://raw.githubusercontent.com/sushisajid/URLs/InGameCharacters/zoe_re7.webp');
+
+insert into InGameCharacters (characterName, backstory, description, englishVA, japaneseVA, motionCapture, spriteURL) values
+('Chris Redfield',
+'american special operations unit operator with bsaa; veteran bioweapon hunter. older brother of claire redfield, mentor to rosemary winters. in the dulvey incident (2017), he responds to blue umbrella’s request to apprehend lucas baker and contain the mold bioweapon. assists ethan winters and mia, eliminates mutant lucas baker, and later coordinates the rescue of zoe baker with her uncle joe.',
+'male, caucasian/american, born c.1973; height varies between 181 - 185 cm (5''11" - 6''1"), mass 80 - 100 kg (176 - 220 lb). blood type o. highly skilled marksman, tactician, and hand-to-hand combatant; specializes in biohazard containment and bioweapon neutralization.',
+'david vaughn', 'hiroki tōchi', 'david vaughn', 'https://raw.githubusercontent.com/sushisajid/URLs/InGameCharacters/chris_re7.webp');
+
+insert into InGameCharacters (characterName, backstory, description, englishVA, japaneseVA, motionCapture, spriteURL) values
+('Alan Droney',
+'special agent for the connections, assigned alongside mia winters to act as handler and parental figure for eveline. during transport on the ss annabelle, eveline’s mold infection overtakes the crew, including alan. after showing symptoms, he sends mia to subdue eveline, but eveline accelerates the mold inside him, killing him. he later appears in a group photo in miranda’s lab in resident evil village.',
+'male, caucasian/american, born c.1961, died october 5, 2014 (aged 53). height 180 cm (5''11"), mass 80 kg (180 lb).',
+'kip pardue', 'kyomitsu mizuuchi', 'mark hobart', 'https://raw.githubusercontent.com/sushisajid/URLs/InGameCharacters/alan_re7.webp');
+
+insert into InGameCharacters (characterName, backstory, description, englishVA, japaneseVA, motionCapture, spriteURL) values
+('Lucas Baker',
+'firstborn of jack and marguerite baker from dulvey, louisiana. infected by eveline’s mold, gains regenerative abilities and a sadistic streak, designing deadly puzzles to kill victims. acted as a researcher for the Connections syndicate, later betrays them and kills their scientists. mutates and is killed by chris redfield in the salt mines after attempting to sell eveline’s bioweapon data.',
+'male, caucasian/american, height 182 cm (6''0"), mass 68 kg (150 lb), deceased. family: jack baker (father), marguerite baker (mother), joe baker (uncle), zoe baker (sister).',
+'jesse pimentel', 'setsuji sato', 'jesse pimentel', 'https://raw.githubusercontent.com/sushisajid/URLs/InGameCharacters/lucas_re7.webp');
+
+insert into InGameCharacters (characterName, backstory, description, englishVA, japaneseVA, motionCapture, spriteURL) values
+('Eveline',
+'codenamed e-001; genetically engineered human bioweapon created by the Connections. possesses mind-control powers, manipulates the baker family and mia winters, abducts humans including ethan winters, and attempts to coerce them into joining her “ideal family.” serves as the main antagonist of resident evil 7: biohazard.',
+'female, height 156 cm (5''1"), mass 45 kg, artificially aged to appear ten years old, deceased july 20, 2017 in dulvey parish, louisiana. first e-type bioweapon; merges with mold upon death.',
+'patricia mcneely', 'yuri tabata', 'N/A', 'https://raw.githubusercontent.com/sushisajid/URLs/InGameCharacters/eveline_re7.webp');
+
+insert into InGameCharacters (characterName, backstory, description, englishVA, japaneseVA, motionCapture, spriteURL) values
+('Jack Baker',
+'infected baker family patriarch; after eveline''s arrival, gained enhanced strength, speed, and near-immortality. along with his family, kidnapped and killed victims to expand eveline''s "family" until his death at the hands of ethan winters.',
+'male, caucasian/american, height 190 cm (6''3"), mass 92 kg, deceased. member of the baker family in dulvey, louisiana.',
+'jack brand', 'kazuhiro yamaji', 'jack brand', 'https://raw.githubusercontent.com/sushisajid/URLs/InGameCharacters/joe_re7.webp');
+
+insert into InGameCharacters (characterName, backstory, description, englishVA, japaneseVA, motionCapture, spriteURL) values
+('Marguerite Baker',
+'infected baker family matriarch; gained ability to control mutant insects, enhanced strength and speed after eveline''s infection. captured victims with family and tried to assimilate ethan and mia winters into eveline''s "family"; ultimately killed by ethan after mutating into a monstrous form.',
+'female, caucasian/american, height 168 cm (5''6"), mass 70 kg, deceased. mother in the baker family of dulvey, louisiana.',
+'sara coates', 'maki izawa', 'sara coates', 'https://raw.githubusercontent.com/sushisajid/URLs/InGameCharacters/marguerite_re7.webp');
+
+insert into InGameCharacters (characterName, backstory, description, englishVA, japaneseVA, motionCapture, spriteURL) values
+('Clancy Jarvis',
+'cameraman for paranormal series Sewer Gators; investigates dulvey haunted house, captured and killed by lucas baker during deadly puzzles.',
+'male, caucasian/american, born c.1982, died june 2, 2017; cameraman; deceased.',
+'Tony Vogel', 'Yu Seki', 'N/A', 'https://raw.githubusercontent.com/sushisajid/URLs/InGameCharacters/clancy_re7.png');
+
+insert into InGameCharacters (characterName, backstory, description, englishVA, japaneseVA, motionCapture, spriteURL) values
+('Peter Walken',
+'tv presenter for paranormal series Sewer Gators; investigates baker ranch with crew, killed by the bakers; death covered as natural gas accident.',
+'male, caucasian/american, born c.1974; tv presenter; deceased.',
+'Robert Vestal', 'N/A', 'N/A', 'https://raw.githubusercontent.com/sushisajid/URLs/InGameCharacters/peter_re7.png');
+
+insert into InGameCharacters (characterName, backstory, description, englishVA, japaneseVA, motionCapture, spriteURL) values
+('Andre Stickland',
+'producer and showrunner of Sewer Gators; captured and murdered by jack baker during investigation of baker ranch; death covered as natural gas accident.',
+'male, caucasian/american, born 1981; tv producer; deceased.',
+'Christopher Ashman', 'N/A', 'N/A', 'https://raw.githubusercontent.com/sushisajid/URLs/InGameCharacters/andre_re7.png');
+
+insert into InGameCharacters (characterName, backstory, description, englishVA, japaneseVA, motionCapture, spriteURL) values
+('Hoffman',
+'civilian victim captured by lucas baker; forced to participate in lucas’ deadly card game "21", endured electrocution and saw attacks, resulting in death.',
+'male, caucasian/american, died june 2017; baker ranch, dulvey parish, louisiana; deceased.',
+'N/A', 'N/A', 'N/A', 'https://raw.githubusercontent.com/sushisajid/URLs/InGameCharacters/hoffman_re7.png');
+
+select * from ingamecharacters;
 select * from appearances;
